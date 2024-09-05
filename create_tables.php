9ee1858980bf098ab1c2e510a063ab74dc8b0a00
maddu rama bhadra rao <ramabhadrarao.maddu@yahoo.com>
2024-09-05 21:49:51 +0530

    Update create_tables.php

--- create_tables.php
+++ create_tables.php
@@ -8,7 +8,7 @@ function prompt($prompt_msg) {
 }
 
 function downloadCRUDGenerator() {
-    $url = "https://raw.githubusercontent.com/ramabhadrarao/mycrudgen/main/CRUDGeneratorv10.php";
+    $url = "https://raw.githubusercontent.com/ramabhadrarao/mycrudgen/main/CRUDGeneratorv11.php";
     $generatorDir = __DIR__ . "/generator";
     if (!is_dir($generatorDir)) {
         mkdir($generatorDir, 0755, true);
